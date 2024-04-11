91f029f9801c2d18e5539a8e72e2a36b6fccb4f1
mattmaomao <matthewcp220@gmail.com>
2024-04-11 23:54:55 +0800

    added game image

--- src/components/itemBlock.js
+++ src/components/itemBlock.js
@@ -1,26 +1,35 @@
-export function ItemBlock({ title, image, date, tag, description }) {
+export function ItemBlock({ title, link, image, date, tag, description }) {
+  const setLink = () => {
+    if (link) {
+      return <a href={link} rel="noreferrer" target="_blank">
+        <strong>{title}</strong>
+      </a>;
+    }
+    else {
+      return <p><strong>{title}</strong></p>
+    }
+  };
+
   return (
     <>
       {/* item block */}
       <div className="things">
         {/* image */}
-        <img className="thing-image" src={"./img/" + image} alt="where's my img :(" />
+        <img
+          className="thing-image"
+          src={"./img/" + image}
+          alt="where's my img :("
+        />
         {/* info */}
         <div className="thing-info">
           {/* title */}
-          <p>
-            <strong>{title}</strong>
-          </p>
+          {setLink()}
           {/* date */}
           <p>{date}</p>
           {/* tag */}
-          <p className="tags">
-            {tag && tag.map((x) => "#" + x + " ")}
-          </p>
+          <p className="tags">{tag && tag.map((x) => "#" + x + " ")}</p>
           {/* short description */}
-          <p className="description">
-            {description}
-          </p>
+          <p className="description">{description}</p>
         </div>
       </div>
     </>
